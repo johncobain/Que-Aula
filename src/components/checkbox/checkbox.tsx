813cb49aeebd659cded1dd1dac119f486532b1ac
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2025-10-09 23:41:44 -0300

    fix: fix responsivity

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import { classNames } from "../../utils/functions/classNames";
 import { ICheckbox } from "./checkbox.interface";
 
 import "./checkbox.style.scss";
+import { useMediaQuery } from "../../utils/functions/useMediaQuery";
 
 const Checkbox = ({ size, selected, onClick }: ICheckbox) => {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useMediaQuery((media) => setIsDesktop(media.matches), 711);
+
   const checkboxClasses = classNames({
     checkbox: true,
-    [`checkbox--${size}`]: true,
+    "checkbox--small": size === "small",
+    "checkbox--large": size === "large" || isDesktop,
     "checkbox--selected": selected,
   });
 
